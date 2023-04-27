0629df320437689499b48ae200e81e0cad87be0e
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-27 16:19:19 -0700

    Complete exercise leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isNotDivBy4 = year % 4 !== 0;
+  const isDivBy100 = year % 100 === 0;
+  const isDivBy400 = year % 400 === 0;
 
+  if (isNotDivBy4) return false;
+  if (isDivBy100) return isDivBy400;
+  return true;
 };
 
 // Do not edit below this line
