0b23d1dbe18f8b6bd3994138fdd7dce2ab9034f6
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-27 14:29:08 -0700

    Complete exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (intBound1, intBound2) {
+  const isInvalidIntBound = function (int) {
+    return !(Number.isInteger(int) && int >= 0);
+  };
+  if (isInvalidIntBound(intBound1) || isInvalidIntBound(intBound2))
+    return "ERROR";
 
+  let start = intBound1 < intBound2 ? intBound1 : intBound2;
+  let end = intBound1 < intBound2 ? intBound2 : intBound1;
+  let res = 0;
+  for (let i = start; i < end + 1; i++) res += i;
+  return res;
 };
 
 // Do not edit below this line
