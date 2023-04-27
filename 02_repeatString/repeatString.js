7e9d98333665561c046206f5ff7a31489e3c01b3
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-27 13:52:08 -0700

    Complete exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  string = String(string);
+  num = parseInt(num);
+  EMPTY_STRING = "";
 
+  if (num === 0) return EMPTY_STRING;
+  if (num < 0) return "ERROR";
+  if (string === EMPTY_STRING) return string;
+
+  let res = [];
+  for (let i = 0; i < num; i++) res.push(string);
+  return res.join("");
 };
 
 // Do not edit below this line
