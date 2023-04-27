9cc366d46b7bd2ceea48de389d4c434c46d578ef
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-27 14:02:06 -0700

    Complete exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringArr = String(string).split("");
+  let l = 0;
+  let r = string.length - 1;
+  while (l < r) {
+    temp = stringArr[l];
+    stringArr[l] = stringArr[r];
+    stringArr[r] = temp;
+    l++;
+    r--;
+  }
+  return stringArr.join("");
 };
 
 // Do not edit below this line
