b63d2023d4bfe7a86594adc9c938f9e0c6167d89
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-28 10:56:13 -0700

    Complete exercise fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number.parseInt(n);
+  if (n <= 0) return "OOPS";
+  if (n <= 2) return 1;
 
+  let nMinus1 = 1;
+  let nMinus2 = 1;
+  let res = -1;
+  for (let i = 3; i < n + 1; i++) {
+    res = nMinus1 + nMinus2;
+    nMinus2 = nMinus1;
+    nMinus1 = res;
+  }
+  return res;
 };
 
 // Do not edit below this line
