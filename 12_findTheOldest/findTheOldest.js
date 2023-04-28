4059a99636f17a7627c1838578bdd0c9a6fb751e
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-28 12:13:58 -0700

    Complete exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  const getAge = (personObj) => {
+    return (
+      (personObj.yearOfDeath
+        ? personObj.yearOfDeath
+        : new Date().getUTCFullYear()) - personObj.yearOfBirth
+    );
+  };
 
+  const maxAge = Math.max(...peopleArray.map((person) => getAge(person)));
+
+  return peopleArray.find((person) => getAge(person) === maxAge);
 };
 
 // Do not edit below this line
