5fc22805187a37567c42d21884f6ffc2a0bac182
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-28 10:03:17 -0700

    Complete exercise calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return Number.parseInt(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return Number.parseInt(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (int) {
+  if (int === 0) return 1;
+  let res = 1;
+  for (let i = 1; i < int + 1; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
