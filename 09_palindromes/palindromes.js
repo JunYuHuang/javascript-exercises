2cdb4cdbc772a5d80e9993ae6b19ab6d5cc8d262
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-04-28 10:31:30 -0700

    Complete exercise palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const sanitizeString = function (string) {
+    return string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  };
 
+  const stringAsArr = sanitizeString(string).split("");
+  let l = 0;
+  let r = stringAsArr.length - 1;
+  while (l < r) {
+    if (stringAsArr[l] !== stringAsArr[r]) return false;
+    l++;
+    r--;
+  }
+  return true;
 };
 
 // Do not edit below this line
